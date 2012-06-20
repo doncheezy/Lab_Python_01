6f884086c683b8bb0977d0f410cb1552b49207a8
Nana Kwesi Baffoe-Mensah <NanaK@Nanas-MacBook-Pro-2.local>
2012-06-20 14:12:06 +0000

    pythonlab2

---
+++ Day2.py
@@ -0,0 +1,14 @@
+'''Exercise 1'''
+print 'x/n,length,NUMBER/n,Android_phone1/n are variable names'
+
+#no. 2 of Exercise 1
+print 'no.1 is boolean\n no.2 float\n No. 3 is String\nNo.4 is int\nNo.5 is string\nNo. 6 is int\n No.7 is a syntax error\nNo.8 is string\nNo.9 is string\nNo. 10 is sting' 
+
+
+
+
+a=False
+b=True
+c=False
+
+b and c
---
+++ userinput.py
@@ -0,0 +1,16 @@
+
+first = raw_input('Enter your first name:')
+
+last = raw_input('Enter your last name:')
+
+print 'Enter your date of birth:'
+
+month = raw_input('Month?')
+
+day= input('Day?')
+year= input('Year?')
+
+
+
+
+print first,last,'was born on',month,day,',',year 
